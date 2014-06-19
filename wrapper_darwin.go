bbba466f4f4752fd60f45d2d2447e336ac105ac1
Helen Tran <contact@helentran.com>
2014-06-19 12:33:12 -0400

    Fix gh-pages commit

--- wrapper_darwin.go
+++
@@ -1,5 +0,0 @@
-package sysv_mq
-
-func cbytesFromStruct(info *_Ctype_struct___msqid_ds_new) uint64 {
-	return uint64(info.msg_cbytes)
-}
--- wrapper_linux.go
+++
@@ -1,5 +0,0 @@
-package sysv_mq
-
-func cbytesFromStruct(info *_Ctype_struct_msqid_ds) uint64 {
-	return uint64(info.__msg_cbytes)
-}
